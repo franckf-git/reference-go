be9db85f29bdd65eaf79f6dfe35eb3c3a4fcec8b
franckf <>
2021-02-07 17:00:55 +0100

    feat: serve views

--- full-projects-tutorials/golang-docs/gowiki/wiki.go
+++ full-projects-tutorials/golang-docs/gowiki/wiki.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 // Page
@@ -28,9 +30,15 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	// The Path is re-sliced with [len("/view/"):] to drop the leading "/view/" component of the request path. This is because the path will invariably begin with "/view/", which is not part of the page's title.
+	title := r.URL.Path[len("/view/"):]
+	// Again, note the use of _ to ignore the error return value from loadPage. This is done here for simplicity and generally considered bad practice.
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/view/", viewHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
