e0d7a30daea583fcde8f27ccb62193d3860cb2ab
franckf <>
2021-02-09 07:29:12 +0100

    feat: use templating

--- full-projects-tutorials/golang-docs/gowiki/wiki.go
+++ full-projects-tutorials/golang-docs/gowiki/wiki.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+    "html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -31,14 +31,24 @@ func loadPage(title string) (*Page, error) {
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	// The Path is re-sliced with [len("/view/"):] to drop the leading "/view/" component of the request path. This is because the path will invariably begin with "/view/", which is not part of the page's title.
-	title := r.URL.Path[len("/view/"):]
-	// Again, note the use of _ to ignore the error return value from loadPage. This is done here for simplicity and generally considered bad practice.
-	p, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+    title := r.URL.Path[len("/view/"):]
+    p, _ := loadPage(title)
+    t, _ := template.ParseFiles("view.html")
+    t.Execute(w, p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/edit/"):]
+    p, err := loadPage(title)
+    if err != nil {
+        p = &Page{Title: title}
+    }
+    t, _ := template.ParseFiles("edit.html")
+    t.Execute(w, p)
 }
 
 func main() {
 	http.HandleFunc("/view/", viewHandler)
+    http.HandleFunc("/edit/", editHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
