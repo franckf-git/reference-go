4de74d0112dfa0d2e8d0ccce6e5c694db939f786
franckf <>
2021-02-10 07:32:17 +0100

    feat: validate entries title

--- full-projects-tutorials/golang-docs/gowiki/wiki.go
+++ full-projects-tutorials/golang-docs/gowiki/wiki.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+    "regexp"
+    "errors"
 )
 
 // Page
@@ -17,7 +19,6 @@ type Page struct {
 }
 
 func (p *Page) save() error {
-    // TODO handle empty field - /edit/ create ".txt"
 	filename := p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
@@ -31,8 +32,22 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+    m := validPath.FindStringSubmatch(r.URL.Path)
+    if m == nil {
+        http.NotFound(w, r)
+        return "", errors.New("invalid Page Title")
+    }
+    return m[2], nil // The title is the second subexpression.
+}
+
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/view/"):]
+    title, err := getTitle(w, r)
+    if err != nil {
+        return
+    }
     p, err := loadPage(title)
     if err != nil {
         http.Redirect(w, r, "/edit/"+title, http.StatusFound)
@@ -42,7 +57,10 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/edit/"):]
+    title, err := getTitle(w, r)
+    if err != nil {
+        return
+    }
     p, err := loadPage(title)
     if err != nil {
         p = &Page{Title: title}
@@ -51,10 +69,13 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/save/"):]
+    title, err := getTitle(w, r)
+    if err != nil {
+        return
+    }
     body := r.FormValue("body")
     p := &Page{Title: title, Body: []byte(body)}
-    err := p.save()
+    err = p.save()
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
