0163e57a7e6d1c9c007a9de5e8c5bbc477ee51bb
franckf <>
2021-03-03 16:13:41 +0100

    test: compare two array

--- snippets/collatz/main_test.go
+++ snippets/collatz/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -16,6 +17,14 @@ func TestProcessNumOdd(t *testing.T) {
 	var want int = 10
 	var msg int = ProcessNum(3)
 	if want != msg {
+		t.Fatalf(`test give us : %v - and we want %v`, msg, want)
+	}
+}
+
+func TestCollatzArray(t *testing.T) {
+	want := []int{8, 4, 2, 1}
+	msg := CollatzProcess(8)
+	if !reflect.DeepEqual(want, msg) {
 		t.Fatalf(`test give us : %v - and we want %v`, msg, want)
 	}
 }
