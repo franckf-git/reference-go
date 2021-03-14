b758ee7f6324f900ce2d2b18702b51bdd6c0a3f0
franckf <>
2021-03-14 08:49:58 +0100

    feat: update array (immutable)

---
+++ snippets/update-array.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {  
+    x := [3]int{1,2,3}
+
+    func(arr [3]int) {
+        arr[0] = 7
+        fmt.Println(arr) //prints [7 2 3]
+    }(x)
+
+    fmt.Println(x) //prints [1 2 3] (not ok if you need [7 2 3])
+}
+
+// If you need to update the original array data use array pointer types.
+
+package main
+
+import "fmt"
+
+func main() {  
+    x := [3]int{1,2,3}
+
+    func(arr *[3]int) {
+        (*arr)[0] = 7
+        fmt.Println(arr) //prints &[7 2 3]
+    }(&x)
+
+    fmt.Println(x) //prints [7 2 3]
+}
