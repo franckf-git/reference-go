6ef8680bdae912323f17927da24b2fef66eb54dc
franckf <>
2021-04-18 08:58:21 +0200

    katas: switch case

---
+++ katas/make-a-function-that-does-arithmetic.go
@@ -0,0 +1,17 @@
+package kata
+
+func Arithmetic(a int, b int, operator string) int {
+	switch operator {
+	case "add":
+		return a + b
+	case "subtract":
+		return a - b
+	case "multiply":
+		return a * b
+	case "divide":
+		return a / b
+	default:
+		return 0
+	}
+}
+
