4c9ab8b85668193056ff69a60b73e666dc360716
franckf <>
2021-06-08 07:09:48 +0200

    feat: async whit buffer to slow down

---
+++ snippets/async/async-channels-buffer.go
@@ -0,0 +1,39 @@
+// if the sender create more that the receiver can get
+
+package main
+
+import (
+    "fmt"
+    "log"
+    "sync"
+    "time"
+)
+
+func slowReceiver(c <-chan int, wg *sync.WaitGroup) {
+    for {
+        n := <-c
+        fmt.Println("received", n)
+        time.Sleep(time.Second)
+        // c <- 42 // THIS WOULD CAUSE AN ERROR "(send to receive-only type <-chan int)"
+        wg.Done()
+    }
+}
+
+func fastSender(c chan<- int, wg *sync.WaitGroup) {
+    for i := 0; i < 5; i++ {
+        c <- i
+        wg.Add(1)
+        fmt.Println(i)
+        // fmt.Println(<-c) // THIS WOULD CAUSE AN ERROR "(receive from send-only type chan<- int)"
+    }
+}
+
+func main() {
+    start := time.Now()
+    var wg sync.WaitGroup
+    c := make(chan int, 3)
+    go slowReceiver(c, &wg)
+    fastSender(c, &wg)
+    wg.Wait()
+    log.Println(time.Since(start))
+}<Paste>
