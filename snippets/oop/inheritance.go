f66f2f27ce8d384f735790e71dc3ad0e0541df85
franckf <>
2021-06-12 07:31:35 +0200

    feat: oop inheritance

---
+++ snippets/oop/inheritance.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type Polygon struct {
+    Sides int
+}
+
+func (p *Polygon) NSides() int {
+    return p.Sides
+}
+
+type Triangle struct {
+    Polygon // anonymous field
+}
+
+func main() {
+    t := Triangle{
+        Polygon{
+            Sides: 3,
+        },
+    }
+    fmt.Println(t.NSides()) // 3
+}
