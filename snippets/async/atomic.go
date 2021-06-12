4469c1795ed87b99bc704572fbcbe2f7159eda94
franckf <>
2021-06-12 06:05:45 +0200

    feat: atomic operation on goroutines

---
+++ snippets/async/atomic.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "fmt"
+    "sync"
+    "sync/atomic"
+)
+
+func f(v *uint32, wg *sync.WaitGroup) {
+    for i := 0; i < 3000; i++ {
+        // *v++ without atomic - routines aren't sync and produce bad results
+        atomic.AddUint32(v, 1)
+    }
+    wg.Done()
+}
+
+func main() {
+    var v uint32 = 42
+    var wg sync.WaitGroup
+    wg.Add(2)
+    go f(&v, &wg)
+    go f(&v, &wg)
+    wg.Wait()
+
+    fmt.Println(v)
+}
