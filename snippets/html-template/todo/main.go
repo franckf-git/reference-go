268e0534aec09c9e2a7ceacddedf21a2e40c609e
franckf <>
2021-06-16 06:48:44 +0200

    feat: todo html templating

---
+++ snippets/html-template/todo/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
+
+func main() {
+	tmpl := template.Must(template.ParseFiles("layout.html"))
+	//tmpl, err := template.ParseFiles("layout.html")
+	//fmt.Println(err)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := TodoPageData{
+			PageTitle: "My TODO list",
+			Todos: []Todo{
+				{Title: "Task 1", Done: false},
+				{Title: "Task 2", Done: true},
+				{Title: "Task 3", Done: true},
+			},
+		}
+		tmpl.Execute(w, data)
+	})
+
+	fmt.Println("server up")
+	http.ListenAndServe(":8080", nil) // port 80 fail
+}
