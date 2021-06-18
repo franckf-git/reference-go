4cc97a95670fe6448478ae78b5be32fcc0872d43
franckf <>
2021-06-18 06:45:28 +0200

    feat: get cpu profiling

--- snippets/maths-problems-examples/fibonacci/run.sh
+++ snippets/maths-problems-examples/fibonacci/run.sh
@@ -1,2 +1,11 @@
 # run
 go run main.go fibo.go binet.go recursive.go
+
+# cpu profile
+go test -cpuprofile cpu.prof -bench recursive_test.go
+# take 5-6 sec
+
+# show profile
+go tool pprof cpu.prof
+# in cmd prompt : show process
+top5 -cum
