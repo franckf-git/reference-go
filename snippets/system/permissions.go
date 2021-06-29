6a66e269becada48004ff806f99e2932957437be
franckf <>
2021-06-29 06:46:15 +0200

    feat: system utils permission and pwd - tidy

---
+++ snippets/system/permissions.go
@@ -0,0 +1,15 @@
+func main() {
+    arguments := os.Args
+    if len(arguments) == 1 {
+        fmt.Println("Please provide an argument!")
+        os.Exit(1)
+    }
+    file := arguments[1]
+    info, err := os.Stat(file)
+    if err != nil {
+        fmt.Println("Error:", err)
+        os.Exit(1)
+    }
+    mode := info.Mode()
+    fmt.Print(file, ": ", mode, "\n")
+}
