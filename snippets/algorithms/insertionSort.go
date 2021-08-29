9df4291ee344d85cfe504f4c4c4c8cbaf7d642f7
franckf <>
2021-08-29 07:50:12 +0200

    feat: implement insertionSort

--- snippets/algorithms/insertionSort.go
+++ snippets/algorithms/insertionSort.go
@@ -1,19 +1,15 @@
-/*
-unsorted_array = [2018, 1998, 1986, 2020, 2006]
-def insertion_sort(array_to_sort):
-    for index in range(1, len(array_to_sort)):
-        current_item = array_to_sort[index]
-        current_left_index = index - 1
-        while current_left_index >= 0 and array_to_sort[current_left_index] > current_item:
-            array_to_sort[current_left_index + 1] = array_to_sort[current_left_index]
-            current_left_index -= 1
-        array_to_sort[current_left_index + 1] = current_item
-    return array_to_sort
-print(insertion_sort(unsorted_array))
-# [1986, 1998, 2006, 2018, 2020]
-*/
 package main
 
 func insertionSort(datas []int) []int {
-	return []int{}
+	datasLen := len(datas)
+	for i := 1; i < datasLen; i++ {
+		itemToSort := datas[i]
+		indexOfPrevItem := i - 1
+		for indexOfPrevItem >= 0 && datas[indexOfPrevItem] > itemToSort {
+			datas[indexOfPrevItem+1] = datas[indexOfPrevItem]
+			indexOfPrevItem--
+		}
+		datas[indexOfPrevItem+1] = itemToSort
+	}
+	return datas
 }
