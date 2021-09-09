9fad386bd8acbb9a9e8560dd353d22336f48f0b8
franckf <>
2021-09-09 05:43:12 +0200

    feat: binary search implement of calhoun

--- snippets/algorithms/binarySearch.go
+++ snippets/algorithms/binarySearch.go
@@ -1,5 +1,26 @@
 package main
 
+// binarySearch clahoun implement no recursif and no new slice
 func binarySearch(numbers []int, search int) int {
+	lo := 0
+	hi := len(numbers) - 1
+
+	for lo <= hi {
+		mid := lo + (hi-lo)/2
+		midValue := numbers[mid]
+
+		if midValue == search {
+			return mid
+		} else if midValue > search {
+			// We want to use the left half of our list
+			hi = mid - 1
+		} else {
+			// We want to use the right half of our list
+			lo = mid + 1
+		}
+	}
+
+	// If we get here we tried to look at an invalid sub-list
+	// which means the number isn't in our list.
 	return -1
 }
