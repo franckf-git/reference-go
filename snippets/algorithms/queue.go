e4406e46f30ebf5acc36b1f7f4ba0366b8b0c66e
franckf <>
2021-09-12 07:09:30 +0200

    feat: simple queue implement

---
+++ snippets/algorithms/queue.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+type Queue struct {
+	slice []int
+}
+
+func (q *Queue) Enqueue(add int) {
+	q.slice = append(q.slice, add)
+}
+
+func (q *Queue) Dequeue() (first int) {
+	first = q.slice[0]
+	q.slice = q.slice[1:len(q.slice)]
+	return
+}
+
+func main() {
+	var myMQ1 Queue
+	// or like :
+	var myMQ2 *Queue = new(Queue)
+	myMQ1.Enqueue(1)
+	myMQ1.Enqueue(1)
+	myMQ2.Enqueue(10)
+	myMQ2.Enqueue(5)
+	myMQ2.Enqueue(6)
+	myMQ2.Enqueue(2)
+	myMQ2.Enqueue(1)
+	myMQ1.Enqueue(1)
+	myMQ2.Enqueue(2)
+	myMQ2.Enqueue(1)
+	myMQ2.Enqueue(8)
+
+	fmt.Println(myMQ1.slice)
+	fmt.Println(myMQ2)
+	fmt.Println(myMQ2.slice)
+
+	myMQ1.Dequeue()
+	myMQ2.Dequeue()
+	myMQ2.Dequeue()
+
+	fmt.Println(myMQ1.slice)
+	fmt.Println(myMQ2.slice)
+
+}
