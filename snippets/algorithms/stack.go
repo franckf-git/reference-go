284aad01422fdfcc4a03f8fbc548015721f6d10f
franckf <>
2021-09-12 07:33:21 +0200

    feat: simple stack implement

---
+++ snippets/algorithms/stack.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+type Stack struct {
+	slice []int
+}
+
+func (s *Stack) Push(add int) {
+	s.slice = append(s.slice, add)
+}
+
+func (s *Stack) Pop() (first int) {
+	first = s.Peek()
+	s.slice = s.slice[:len(s.slice)-1]
+	return
+}
+
+func (s *Stack) Peek() int {
+	return s.slice[len(s.slice)-1]
+}
+
+func main() {
+	var myST1 Stack
+	// or like :
+	var myST2 *Stack = new(Stack)
+	myST1.Push(1)
+	myST1.Push(2)
+	myST2.Push(10)
+	myST2.Push(5)
+	fmt.Println(myST2.Peek())
+	myST2.Push(6)
+	myST2.Push(2)
+	myST2.Push(1)
+	myST1.Push(3)
+	myST2.Push(2)
+	fmt.Println(myST2.Peek())
+	myST2.Push(1)
+	myST2.Push(8)
+	fmt.Println(myST2.Peek())
+
+	fmt.Println(myST1.slice)
+	fmt.Println(myST2)
+	fmt.Println(myST2.slice)
+
+	myST1.Pop()
+	myST2.Pop()
+	myST2.Pop()
+
+	fmt.Println(myST1.slice)
+	fmt.Println(myST2.slice)
+
+}
