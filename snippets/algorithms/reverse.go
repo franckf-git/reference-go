90db5ea0ef9b21df4e5f403e683c71e5fa6fc692
franckf <>
2022-01-24 21:42:52 +0100

    feat: add reverse string and array -ish

---
+++ snippets/algorithms/reverse.go
@@ -0,0 +1,53 @@
+package main
+
+func main() {
+	start := "😎⚽"
+	end := reverseRune2(start)
+	print(end)
+	print("\n")
+}
+
+func reverseRune(in string) string {
+	runes := []rune(in)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[j], runes[i] = runes[i], runes[j]
+	}
+	return string(runes)
+}
+
+func reverseRune2(in string) string {
+	runes := []rune(in)
+	for i := 0; i < len(runes)-1; i = i + 1 {
+		for j := len(runes) - 1; i < j; j = j - 1 {
+			runes[j], runes[i] = runes[i], runes[j]
+		}
+	}
+	return string(runes)
+}
+
+func reverseRuneSplit(in string) string {
+	runes := []rune(in)
+	for i := 0; i < len(runes)/2; i++ {
+		j := len(runes) - i - 1
+		runes[j], runes[i] = runes[i], runes[j]
+	}
+	return string(runes)
+}
+
+func reverseRuneInefficient(in string) (out string) {
+	runes := []rune(in)
+	var runesOut []rune
+	for i := len(runes) - 1; i >= 0; i-- {
+		runesOut = append(runesOut, runes[i])
+	}
+	out = string(runesOut)
+	return
+}
+
+func reverseInefficient(in string) (out string) {
+	for _, v := range in {
+		out = string(v) + out
+	}
+	return
+}
+
