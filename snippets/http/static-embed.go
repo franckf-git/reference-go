052191cbc3ffb8fba34cf6a6bcbeeda3757d9ef0
franckf <>
2022-04-04 16:32:56 +0200

    feat: embed filesystem serve has static

---
+++ snippets/http/static-embed.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"embed"
+	"log"
+	"net/http"
+)
+
+//go:embed internal/embedtest/testdata/*.txt
+var content embed.FS
+
+func main() {
+	mutex := http.NewServeMux()
+	//mutex.Handle("/", http.FileServer(http.FS(content)))
+	mutex.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(content))))
+	err := http.ListenAndServe(":8080", mutex)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
