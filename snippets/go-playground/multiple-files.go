26ce6d99505cad97ffcaae1a8703aaef190cc584
franckf <>
2022-05-22 15:10:35 +0200

    feat: add example from go playground

---
+++ snippets/go-playground/multiple-files.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"play.ground/foo"
+)
+
+func main() {
+	foo.Bar()
+}
+-- go.mod --
+module play.ground
+-- foo/foo.go --
+package foo
+
+import "fmt"
+
+func Bar() {
+	fmt.Println("This function lives in an another file!")
+}
+
