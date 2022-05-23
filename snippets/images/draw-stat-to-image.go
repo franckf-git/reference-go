2623ef7a16595b9973d0141ce7afc553a0c04874
franckf <4699125-franckf@users.noreply.gitlab.com>
2022-05-23 04:35:47 +0000

    fix: to value with more colors

--- snippets/images/draw-stat-to-image.go
+++ snippets/images/draw-stat-to-image.go
@@ -14,12 +14,58 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
+const (
+	r int = iota
+	g
+	b
+)
+
 func main() {
 	var datas [100]int
 	for i := range datas {
 		datas[i] = rand.Intn(100)
 	}
 	datasToPng(datas)
+}
+
+func transform(nums [100]int) (colrs [100]color.RGBA) {
+	var biggest int
+	for _, v := range nums {
+		if v >= biggest {
+			biggest = v
+		}
+	}
+
+	var curColrs color.RGBA
+	for i, v := range nums {
+		cur := (float64(v) / float64(biggest)) * 255
+		rng := rand.Intn(3)
+		switch rng {
+		case r:
+			curColrs = color.RGBA{
+				R: uint8(cur),
+				G: uint8(255),
+				B: uint8(255),
+				A: 255,
+			}
+		case g:
+			curColrs = color.RGBA{
+				R: uint8(255),
+				G: uint8(cur),
+				B: uint8(255),
+				A: 255,
+			}
+		case b:
+			curColrs = color.RGBA{
+				R: uint8(255),
+				G: uint8(255),
+				B: uint8(cur),
+				A: 255,
+			}
+		}
+		colrs[i] = curColrs
+	}
+	return
 }
 
 func datasToPng(datas [100]int) {
@@ -39,7 +85,7 @@ func datasToPng(datas [100]int) {
 				},
 			}
 
-			coloring := image.Uniform{valueToRGB(datasMatrixed[line][col])}
+			coloring := image.Uniform{datasMatrixed[line][col]}
 
 			draw.Draw(workingImage, rect, &coloring, image.Point{}, draw.Src)
 		}
@@ -52,11 +98,12 @@ func datasToPng(datas [100]int) {
 	png.Encode(exportPNG, workingImage)
 }
 
-func matrix(datas [100]int) (matrix [10][10]int) {
+func matrix(datas [100]int) (matrix [10][10]color.RGBA) {
+	datasColored := transform(datas)
 	var delta int
 	for line := range matrix {
 		for col := range matrix[line] {
-			matrix[line][col] = datas[delta]
+			matrix[line][col] = datasColored[delta]
 			delta++
 		}
 	}
