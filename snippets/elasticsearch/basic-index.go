196625842a363495cb2fac89b2ab05ffe235ee8e
franckf <>
2022-06-05 08:26:44 +0200

    feat: basic index

---
+++ snippets/elasticsearch/basic-index.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/elastic/elastic-transport-go/v8/elastictransport"
+	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v8/esapi"
+)
+
+func main() {
+	cert, _ := ioutil.ReadFile("ca.crt")
+	cfg := elasticsearch.Config{
+		Logger:            &elastictransport.CurlLogger{Output: os.Stdout, EnableRequestBody: true, EnableResponseBody: true},
+		Addresses:         []string{"https://localhost:9200"},
+		Username:          "elastic",
+		Password:          "3lastic",
+		EnableDebugLogger: true,
+		CACert:            cert,
+	}
+
+	es, err := elasticsearch.NewClient(cfg)
+	if err != nil {
+		log.Fatalf("Error creating the client: %s", err)
+	}
+
+	for i, v := range []string{"test 1", "test 2"} {
+		data, _ := json.Marshal(struct{ Title string }{Title: v})
+		req := esapi.IndexRequest{
+			Index:      "test_part_1",
+			DocumentID: strconv.Itoa(i + 1),
+			Body:       bytes.NewReader(data),
+			Refresh:    "true",
+		}
+		res, err := req.Do(context.Background(), es)
+		if err != nil {
+			log.Fatalf("Error res indexing: %s", err)
+		}
+		defer res.Body.Close()
+		if res.IsError() {
+			log.Fatalf("Error indexing: %s", err)
+		}
+		var r map[string]interface{}
+		err = json.NewDecoder(res.Body).Decode(&r)
+		if err != nil {
+			log.Fatalf("Error res decoding: %s", err)
+		}
+		log.Printf("response: %#+v \n result: %#+v", res.Status(), r["result"])
+	}
+
+	for i, v := range []string{"test 3", "test 4"} {
+		var b strings.Builder
+		b.WriteString(`{"title":"`)
+		b.WriteString(v)
+		b.WriteString(`"}`)
+		req := esapi.IndexRequest{
+			Index:      "test_part_2",
+			DocumentID: strconv.Itoa(i + 1),
+			Body:       strings.NewReader(b.String()),
+			Refresh:    "true",
+		}
+		res, err := req.Do(context.Background(), es)
+		if err != nil {
+			log.Fatalf("Error res indexing: %s", err)
+		}
+		defer res.Body.Close()
+		if res.IsError() {
+			log.Fatalf("Error indexing: %s", err)
+		}
+		var r map[string]interface{}
+		err = json.NewDecoder(res.Body).Decode(&r)
+		if err != nil {
+			log.Fatalf("Error res decoding: %s", err)
+		}
+		log.Printf("response: %#+v \n result: %#+v", res.Status(), r["result"])
+	}
+}
