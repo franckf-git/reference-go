befd1a35dca91c578772073536184f8357c2fdc6
franckf <>
2022-07-27 08:04:39 +0200

    feat: end of first try for count kata #16 (visualize not perfect)

--- katas/count-word.go
+++ katas/count-word.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"regexp"
 	"strings"
 )
 
 func main() {
 	var osStdin = os.Stdin
-	log.Printf("cw: %#+v\n", countWord(osStdin))
+	visualize(sortByValues(countWord(osStdin)))
 }
 
 func countWord(input io.Reader) map[string]int {
@@ -19,8 +21,9 @@ func countWord(input io.Reader) map[string]int {
 	scanner.Split(bufio.ScanWords) // feel like cheating
 	for scanner.Scan() {
 		currentWord := scanner.Text()
-		currentWord = strings.ToLower(currentWord)
-		results[currentWord] = results[currentWord] + 1
+		if validateWord(&currentWord) {
+			results[currentWord] = results[currentWord] + 1
+		}
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
@@ -28,6 +31,73 @@ func countWord(input io.Reader) map[string]int {
 	return results
 }
 
-// TODO use regex for simple and complex punctuations ?
-// TODO accents à ?
-// TODO sort response
+func validateWord(word *string) bool {
+	*word = strings.ToLower(*word)
+	matched, err := regexp.Match(`^[[:alnum:]]+$`, []byte(*word))
+	if err != nil {
+		log.Fatal(err)
+	}
+	if !matched {
+		if len(*word) == 1 {
+			return false
+		}
+		re := regexp.MustCompile(`[^\w|àâçéèêëîïôûùüÿñæœ]$`)
+		*word = re.ReplaceAllString(*word, "")
+	}
+	if *word == "" {
+		return false
+	}
+	return true
+}
+
+func sortByValues(inputs map[string]int) []Counter {
+	unsorted := []Counter{}
+	for word, count := range inputs {
+		unsorted = append(unsorted, Counter{
+			word:  word,
+			count: count,
+		})
+	}
+	sorted := quickSort(unsorted)
+	return sorted
+}
+
+type Counter struct {
+	word  string
+	count int
+}
+
+// this is really cheating, get one of my old quick sort
+func quickSort(datas []Counter) []Counter {
+	if len(datas) == 0 {
+		return []Counter{}
+	}
+	pivot := datas[len(datas)-1]
+	smallerValues := []Counter{}
+	largerValues := []Counter{}
+	for index := 0; index < len(datas)-1; index++ {
+		if datas[index].count > pivot.count {
+			smallerValues = append(smallerValues, datas[index])
+		} else {
+			largerValues = append(largerValues, datas[index])
+		}
+	}
+	qsSmallerPivot := append(quickSort(smallerValues), pivot)
+	return append(qsSmallerPivot, quickSort(largerValues)...)
+}
+
+func visualize(datas []Counter) {
+	var biggerSize int
+	for _, data := range datas {
+		if len(data.word) > biggerSize {
+			biggerSize = len(data.word)
+		}
+	}
+
+	fmt.Print("+", "--", strings.Repeat("-", biggerSize), "--", "+", "-------", "+", "\n")
+	fmt.Print("+ ", "word", strings.Repeat(" ", biggerSize-2), " + ", "count", " +", "\n")
+	for _, data := range datas {
+		fmt.Print("+ ", data.word, strings.Repeat(" ", biggerSize-len(data.word)), "   +   ", data.count, "   +", "\n")
+	}
+	fmt.Print("+", "--", strings.Repeat("-", biggerSize), "--", "+", "-------", "+", "\n")
+}
--- katas/count-word_test.go
+++ katas/count-word_test.go
@@ -33,15 +33,60 @@ func Test_countWord(t *testing.T) {
 		{
 			name: "simple punctuation",
 			args: args{
-				input: strings.NewReader("un Deux, Trois : deux TROIS trois."),
+				input: strings.NewReader("un Deux, Trois : deux TROIS trois. l'avion"),
 			},
-			wantResults: map[string]int{"un": 1, "deux": 2, "trois": 3},
+			wantResults: map[string]int{"un": 1, "deux": 2, "trois": 3, `l'avion`: 1},
+		},
+		{
+			name: "multilines",
+			args: args{
+				input: strings.NewReader(`
+				Maître Renard par l’odeur alléché
+				Lui tint à peu près ce langage :
+				`),
+			},
+			wantResults: map[string]int{"alléché": 1, "ce": 1, "langage": 1, "lui": 1, "l’odeur": 1, "maître": 1, "par": 1, "peu": 1, "près": 1, "renard": 1, "tint": 1, "à": 1},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if gotResults := countWord(tt.args.input); !reflect.DeepEqual(gotResults, tt.wantResults) {
 				t.Errorf("countWord() = %v, want %v", gotResults, tt.wantResults)
+			}
+		})
+	}
+}
+
+func Test_sortByValues(t *testing.T) {
+	type args struct {
+		inputs map[string]int
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantSorted []Counter
+	}{
+		{
+			name: "simple sort",
+			args: args{
+				inputs: map[string]int{"un": 1, "deux": 2, "trois": 3},
+			},
+			wantSorted: []Counter{{
+				word:  "trois",
+				count: 3,
+			}, {
+				word:  "deux",
+				count: 2,
+			}, {
+				word:  "un",
+				count: 1,
+			}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotSorted := sortByValues(tt.args.inputs); !reflect.DeepEqual(gotSorted, tt.wantSorted) {
+				t.Errorf("sortByValues() = %v, want %v", gotSorted, tt.wantSorted)
 			}
 		})
 	}
