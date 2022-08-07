9a479da43aff2cd66d34b87011a7f72d9b0507fb
franckf <>
2022-08-07 16:20:34 +0200

    refacto test: #13 tests both ways share TestTable

--- katas/roman-numeral_test.go
+++ katas/roman-numeral_test.go
@@ -5,69 +5,57 @@ import (
 	"testing"
 )
 
+var tests = []struct {
+	Num   int
+	Roman string
+}{
+	{1, "I"},
+	{2, "II"},
+	{3, "III"},
+	{4, "IV"},
+	{5, "V"},
+	{6, "VI"},
+	{7, "VII"},
+	{8, "VIII"},
+	{9, "IX"},
+	{10, "X"},
+	{14, "XIV"},
+	{18, "XVIII"},
+	{20, "XX"},
+	{39, "XXXIX"},
+	{40, "XL"},
+	{47, "XLVII"},
+	{49, "XLIX"},
+	{50, "L"},
+	{69, "LXIX"},
+	{100, "C"},
+	{90, "XC"},
+	{400, "CD"},
+	{500, "D"},
+	{900, "CM"},
+	{1000, "M"},
+	{1984, "MCMLXXXIV"},
+	{3999, "MMMCMXCIX"},
+	{2014, "MMXIV"},
+	{1006, "MVI"},
+	{798, "DCCXCVIII"},
+}
+
 func Test_num2rom(t *testing.T) {
-	tests := []struct {
-		inNum     int
-		wantRoman string
-	}{
-		{1, "I"},
-		{2, "II"},
-		{3, "III"},
-		{4, "IV"},
-		{5, "V"},
-		{6, "VI"},
-		{7, "VII"},
-		{8, "VIII"},
-		{9, "IX"},
-		{10, "X"},
-		{14, "XIV"},
-		{18, "XVIII"},
-		{20, "XX"},
-		{39, "XXXIX"},
-		{40, "XL"},
-		{47, "XLVII"},
-		{49, "XLIX"},
-		{50, "L"},
-		{69, "LXIX"},
-		{100, "C"},
-		{90, "XC"},
-		{400, "CD"},
-		{500, "D"},
-		{900, "CM"},
-		{1000, "M"},
-		{1984, "MCMLXXXIV"},
-		{3999, "MMMCMXCIX"},
-		{2014, "MMXIV"},
-		{1006, "MVI"},
-		{798, "DCCXCVIII"},
-	}
 	for _, tt := range tests {
-		t.Run(fmt.Sprint(tt.inNum), func(t *testing.T) {
-			if gotRoman := num2rom(tt.inNum); gotRoman != tt.wantRoman {
-				t.Errorf("num2rom() = %v, want %v", gotRoman, tt.wantRoman)
+		t.Run(fmt.Sprint(tt.Num), func(t *testing.T) {
+			if gotRoman := num2rom(tt.Num); gotRoman != tt.Roman {
+				t.Errorf("num2rom() = %v, want %v", gotRoman, tt.Roman)
 			}
 		})
 	}
 }
 
 func Test_rom2num(t *testing.T) {
-	tests := []struct {
-		wantNum int
-		inRoman string
-	}{
-		{1, "I"},
-		{2, "II"},
-		{3, "III"},
-		{4, "IV"},
-		{5, "V"},
-		{6, "VI"},
-		{7, "VII"},
-		{8, "VIII"},
-	}
-	for _, tt := range tests {
-		t.Run(fmt.Sprint(tt.inRoman), func(t *testing.T) {
-			if gotNum := rom2num(tt.inRoman); gotNum != tt.wantNum {
-				t.Errorf("num2rom() = %v, want %v", gotNum, tt.wantNum)
+	for _, tt := range tests[:5] {
+		t.Run(fmt.Sprint(tt.Roman), func(t *testing.T) {
+			if gotNum := rom2num(tt.Roman); gotNum != tt.Num {
+				t.Errorf("num2rom() = %v, want %v", gotNum, tt.Num)
 			}
 		})
 	}
