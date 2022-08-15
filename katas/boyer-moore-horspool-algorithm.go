10acebd8adfdca5d43bd2566a698f2db8507abaf
franckf <>
2022-08-15 06:55:58 +0200

    katas: working example

--- katas/boyer-moore-horspool-algorithm.go
+++ katas/boyer-moore-horspool-algorithm.go
@@ -1,37 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 // https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore%E2%80%93Horspool_algorithm
 func main() {
-	fmt.Println(bhm("wikipedia", "wiki"))
+	fmt.Println(bhm("wikipedia", "edia"))
 }
 
 func bhm(source, search string) (out int) {
-	var alphaSizeAscii int = 256
-	var skipTable = make([]int, alphaSizeAscii)
+	var alphaSizeAscii [256]int
 	var sizeSource int = len(source)
 	var sizeSearch int = len(search)
+	var iSource int
+	var iSearch int
 
 	// preprocess
-	for i := 0; i < alphaSizeAscii; i++ {
-		skipTable[i] = sizeSearch
+	// - init
+	for i := 0; i < len(alphaSizeAscii); i++ {
+		alphaSizeAscii[i] = sizeSearch
 	}
-	for i := 0; i < sizeSearch; i++ {
-		skipTable[i] = sizeSearch - 1 - i
+	log.Printf("alphaSizeAscii-init: %#+v\n", alphaSizeAscii)
+	// - build
+	for i := 0; i < (sizeSearch - 1); i++ {
+		alphaSizeAscii[search[i]] = (sizeSearch - 1) - i
 	}
+	log.Printf("alphaSizeAscii-build: %#+v\n", alphaSizeAscii)
 
+	log.Printf("iSource-before-search: %#+v\n", iSource)
+	log.Printf("iSearch-before-search: %#+v\n", iSearch)
 	// search
-	for {
-		if source[out+sizeSearch-1] == search[sizeSearch-1] {
-			return
+	for (iSource + sizeSearch) <= sizeSource {
+		iSearch = sizeSearch - 1
+		log.Printf("iSource-begin-loop: %#+v\n", iSource)
+		log.Printf("iSearch-begin-loop: %#+v\n", iSearch)
+		for source[iSource+iSearch] == search[iSearch] {
+			if iSearch == 0 {
+				out = iSource
+				return
+			}
+			log.Printf("iSource-after-if: %#+v\n", iSource)
+			log.Printf("iSearch-after-if: %#+v\n", iSearch)
+			iSearch--
 		}
-
-		out = out + skipTable[source[out+sizeSearch-1]]
-
-		if out <= sizeSource-sizeSearch {
-			return
-		}
+		iSource = iSource + alphaSizeAscii[source[iSource+iSearch]]
+		log.Printf("iSource-after-loop: %#+v\n", iSource)
+		log.Printf("iSearch-after-loop: %#+v\n", iSearch)
 	}
 	out = -1
 	return
